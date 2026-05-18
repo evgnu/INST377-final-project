4415bd52b13f4d335ebd2cec31230e38582737a9
Evan Nuesca <noobatgames65@gmail.com>
2026-05-17 23:20:11 -0400

    test 9

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const supabaseClient = require('@supabase/supabase-js');
 const dotenv = require('dotenv');
-const path = require('path');
+
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
-app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile('public/home.html', { root: __dirname});
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
